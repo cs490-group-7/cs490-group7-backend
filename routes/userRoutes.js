60046742d864038c964f9ba0611c06da3c208816
ngutierrez45 <nag45@njit.edu>
2023-11-06 13:30:47 -0500

    error handling

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,52 +6,52 @@ const router = express.Router();
 
 // Registration Endpoint
 router.post('/register', async (req, res) => {
-    const { firstName, lastName, email, password, isCoach } = req.body;
+  const { firstName, lastName, email, password, isCoach } = req.body;
 
-    try {
-        // Check if user already exists
-        const userExists = await db_conn.query('SELECT email FROM Users WHERE email = ?', [email]);
-        if (userExists.length > 0) {
-            return res.status(400).json({ message: "User already exists" });
-        }
-
-        // Hash the password
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-
-        // Insert user into the database
-        const userType = isCoach ? 'Coach' : 'Client';
-        await db_conn.query('INSERT INTO Users (first_name, last_name, email, password, user_type) VALUES (?, ?, ?, ?, ?)', [firstName, lastName, email, hashedPassword, userType]);
-
-        res.status(201).json({ message: "User registered successfully" });
-    } catch (error) {
-        console.error('Registration error:', error);
-        res.status(500).json({ message: "Server error" });
+  try {
+    // Check if user already exists
+    const userExists = await db_conn.query('SELECT email FROM Users WHERE email = ?', [email]);
+    if (userExists.length > 0) {
+      return res.status(400).json({ message: "User already exists" });
     }
+
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Insert user into the database
+    const userType = isCoach ? 'Coach' : 'Client';
+    await db_conn.query('INSERT INTO Users (first_name, last_name, email, password, user_type) VALUES (?, ?, ?, ?, ?)', [firstName, lastName, email, hashedPassword, userType]);
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    console.error('Registration error:', error);
+    res.status(500).json({ message: "Server error" });
+  }
 });
 
 // Login Endpoint
 router.post('/login', async (req, res) => {
-    const { email, password } = req.body;
-   console.log("reached");
-    try {
-        // Check if user exists
-        const user = await db_conn.query('SELECT * FROM Users WHERE email = ?', [email]);
-        if (user.length === 0) {
-            return res.status(400).json({ message: "Invalid email or password" });
-        }
+  const { email, password } = req.body;
 
-        // Compare passwords
-        const validPassword = await bcrypt.compare(password, user[0].password);
-        if (!validPassword) {
-            return res.status(400).json({ message: "Invalid email or password" });
-        }
-
-        res.status(200).json({ message: "Logged in successfully" });
-    } catch (error) {
-        console.error('Login error:',error);
-        res.status(500).json({ message: "Server error" });
+  try {
+    // Check if user exists
+    const user = await db_conn.query('SELECT * FROM Users WHERE email = ?', [email]);
+    if (user.length === 0) {
+      return res.status(400).json({ message: "Invalid email or password" });
     }
+
+    // Compare passwords
+    const validPassword = await bcrypt.compare(password, user[0].password);
+    if (!validPassword) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    res.status(200).json({ message: "Logged in successfully" });
+  } catch (error) {
+    console.error('Login error:', error);
+    res.status(500).json({ message: "Server error" });
+  }
 });
 
 module.exports = router;
