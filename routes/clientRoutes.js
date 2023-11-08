89ac167bd501eb87b128cea52c9cbae0b239ce45
Adem Avci <aa2822@njit.edu>
2023-11-08 13:10:33 -0500

    Updated mock-data and routes for mock data

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -3,5 +3,13 @@ const router = express.Router();
 const clientController = require('../controllers/clientController');
 
 router.post('/initial-survey', clientController.addInitialSurvey);
-
+router.get('../mock-data', (req, res) => {
+    try {
+      const mockData = JSON.parse(fs.readFileSync(mockDataFilePath, 'utf8'));
+      res.json(mockData);
+    } catch (error) {
+      console.error('Error reading mock data:', error);
+      res.status(500).json({ error: 'Failed to retrieve mock data' });
+    }
+  });
 module.exports = router;
