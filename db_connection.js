29ac2e0beab5cd89803de60857813f937792d131
Jumaiza <zaid.abujumaiza@gmail.com>
2023-11-14 15:35:00 -0500

    add port to db connection

--- db_connection.js
+++ db_connection.js
@@ -12,6 +12,7 @@ const dbConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  port: process.env.PORT,
 };
 const connection = mysql.createConnection(dbConfig);
 
