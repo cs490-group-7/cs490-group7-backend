5f00895f31812524f038874a5957eae4d9cffadf
Jumaiza <zaid.abujumaiza@gmail.com>
2023-11-24 02:28:36 -0500

    use user_id to get progress

--- routes/progressRoutes.js
+++ routes/progressRoutes.js
@@ -3,21 +3,21 @@ const db_conn = require('../db_connection');
 
 const router = express.Router();
 
-router.get('/progress-data', async (req, res) => {
-  try {
-      const exerciseQuery = 'SELECT date, weight, calorie_intake, water_intake FROM DailySurvey WHERE user_id = 1';
-
-      const results = await new Promise((resolve, reject) => {
-          db_conn.query(exerciseQuery, (error, results, fields) => {
-              if (error) reject(error);
-              else resolve(results);
-          });
-      });
-      res.json(results);
-  } catch (error) {
-      console.error('Data retrieval error:', error);
-      res.status(500).json({ message: "Server error" });
-  }
+router.post('/progress-data', async (req, res) => {
+    const userId = req.body.userId;
+    try {
+        const query = 'SELECT date, weight, calorie_intake, water_intake FROM DailySurvey WHERE user_id = ?';
+        const results = await new Promise((resolve, reject) => {
+            db_conn.query(query, [userId], (error, results, fields) => {
+                if (error) reject(error);
+                else resolve(results);
+            });
+        });
+        res.json(results);
+    } catch (error) {
+        console.error('Data retrieval error:', error);
+        res.status(500).json({ message: "Error retrieving progress data" });
+    }
 });
 
 module.exports = router;
