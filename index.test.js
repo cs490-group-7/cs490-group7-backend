5e08fd95de11b4e148f78903ae0dd47e1b5362dd
Jumaiza <zaid.abujumaiza@gmail.com>
2023-11-29 00:51:58 -0500

    exit after finishing tests

--- index.test.js
+++ index.test.js
@@ -3,8 +3,8 @@ const app = require('./index');
 
 describe("GET /health/check", () => {
     it("should return hello from server", async () => {
-      const res = await request(app).get("/health/check");
-      expect(res.statusCode).toBe(200);
-      expect(res.body.message).toBe("Hello World From Server!")
+        const res = await request(app).get("/health/check");
+        expect(res.statusCode).toBe(200);
+        expect(res.body.message).toBe("Hello World From Server!")
     });
-  });
+});
