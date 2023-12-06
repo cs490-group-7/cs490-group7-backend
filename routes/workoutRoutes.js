527be74f8764a7353648a7ef8760ffb4c79204c5
ASiracusa <fedorafilmz@gmail.com>
2023-12-05 23:47:47 -0500

    Log Session, Updating on Calendar

--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -250,6 +250,37 @@ router.post('/get-assignments', async (req, res) => {
     }
 });
 
+router.post('/get-todays-logs', async (req, res) => {
+    const { userId } = req.body;
+
+    try {
+        // Check if user exists
+        const checkUserQuery = 'SELECT id FROM Users WHERE id=?';
+        const userExists = await new Promise((resolve, reject) => {
+            db_conn.query(checkUserQuery, [userId], (error, results) => {
+                if (error) reject(error);
+                else resolve(results);
+            });
+        });
+        if (userExists.length === 0) {
+            return res.status(400).json({ message: "User does not exist: " + userId.toString() });
+        }
+
+        // Get the logs for today for the user
+        const logQuery = 'SELECT workout_id FROM WorkoutSession WHERE user_id=? AND session_date=?';
+        const results = await new Promise((resolve, reject) => {
+            db_conn.query(logQuery, [userId, (new Date()).toISOString().split('T')[0]], (error, results, fields) => {
+                if (error) reject(error);
+                else resolve(results);
+            });
+        });
+        res.json(results);
+    } catch (error) {
+        console.error('Log retrieval error:', error);
+        res.status(500).json({ message: "Server error" });
+    }
+});
+
 router.post('/unassign-workout', async (req, res) => {
     const { workoutId, userId, dayOfWeek } = req.body;
 
@@ -266,6 +297,75 @@ router.post('/unassign-workout', async (req, res) => {
     } catch (error) {
         console.error('Assignment removal error:', error);
         res.status(500).json({ message: "Server error" });
+    }
+});
+
+router.post('/log-session', async (req, res) => {
+    const { userId, workoutId, sessionDate, dayOfWeek } = req.body;
+  
+    try {
+        // Check if user exists
+        const checkUserQuery = 'SELECT id FROM Users WHERE id=?';
+        const userExists = await new Promise((resolve, reject) => {
+            db_conn.query(checkUserQuery, [userId], (error, results) => {
+                if (error) reject(error);
+                else resolve(results);
+            });
+        });
+        if (userExists.length === 0) {
+            return res.status(400).json({ message: "User does not exist: " + userId.toString() });
+        }
+
+        // Check if workout exists
+        const checkWorkoutQuery = 'SELECT workout_id FROM Workout WHERE workout_id=? AND creator_id=?';
+        const workoutExists = await new Promise((resolve, reject) => {
+            db_conn.query(checkWorkoutQuery, [workoutId, userId], (error, results) => {
+                if (error) reject(error);
+                else resolve(results);
+            });
+        });
+        if (workoutExists.length === 0) {
+            return res.status(400).json({ message: "Workout does not exist or is not owned by this user: workout: " + workoutId.toString() + ", user: " + userId.toString() });
+        }
+
+        // Check if assignment exists
+        const checkAssignmentQuery = 'SELECT workout_id FROM WorkoutCalendar WHERE workout_id=? AND user_id=? AND day_of_week=?';
+        const assignmentExists = await new Promise((resolve, reject) => {
+            db_conn.query(checkAssignmentQuery, [workoutId, userId, dayOfWeek], (error, results) => {
+                if (error) reject(error);
+                else resolve(results);
+            });
+        });
+        if (assignmentExists.length === 0) {
+            return res.status(400).json({ message: "Workout is not scheduled for today. " });
+        }
+
+        // Check if session was already logged for today
+        const checkSessionQuery = 'SELECT workout_id FROM WorkoutSession WHERE workout_id=? AND user_id=? AND session_date=?';
+        const sessionExists = await new Promise((resolve, reject) => {
+            db_conn.query(checkSessionQuery, [workoutId, userId, sessionDate], (error, results) => {
+                if (error) reject(error);
+                else resolve(results);
+            });
+        });
+        if (sessionExists.length > 0) {
+            return res.status(400).json({ message: "Workout session has already been logged today. " });
+        }
+
+        // Log the session
+        await db_conn.query('INSERT INTO WorkoutSession (workout_id, user_id, session_date) VALUES (?, ?, ?)', [workoutId, userId, sessionDate]);
+        await new Promise((resolve, reject) => {
+            db_conn.query(checkSessionQuery, [workoutId, userId, dayOfWeek], (error, results) => {
+                if (error) reject(error);
+                else resolve(results);
+            });
+        });
+        res.status(201).json({ message: "Session logged successfully" });
+
+    } catch (error) {
+      console.error('Log session error:', error);
+  
+      res.status(500).json({ message: "Server error" });
     }
 });
 
