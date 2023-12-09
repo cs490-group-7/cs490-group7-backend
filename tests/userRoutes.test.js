00d02d24613d5e0d7f17e5310b3d1f3062c12485
ngutierrez45 <nag45@njit.edu>
2023-12-09 17:38:25 -0500

    fixed removing test user after userRoutes test

--- tests/userRoutes.test.js
+++ tests/userRoutes.test.js
@@ -1,11 +1,28 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('../index'); 
+const db_conn = require('../db_connection'); 
 
 chai.use(chaiHttp);
 const expect = chai.expect;
 
 describe('User Routes', () => {
+
+  after((done) => {
+    // Delete the user from the database after the test
+    const deleteUserQuery = 'DELETE FROM Users WHERE email = ?';
+    const values = ['joe.doe@example.com'];
+
+    db_conn.query(deleteUserQuery, values, (error, results) => {
+      if (error) {
+        console.error('Error tearing down test user:', error);
+        done(error);
+      } else {
+        done();
+      }
+    });
+  });
+
   it('should register a new user', (done) => {
     chai.request(app)
       .post('/api/users/register')
