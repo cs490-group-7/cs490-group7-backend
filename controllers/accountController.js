88b5593d73b4567e3f74d3504f6fa45ba481bc42
ngutierrez45 <nag45@njit.edu>
2023-12-11 19:15:50 -0500

    better architecture for accountSettings

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,45 +1,53 @@
 const db_conn = require('../db_connection');
 
-class accountController {
-  async getAccountInfo(req, res) {
-    const userId = req.body.userId;
-    const query = 'SELECT first_name, last_name, email, phone from Users WHERE id = ?;';
-    try {
-      const result = await this.executeQuery(query, [userId]);
-      res.status(200).json(result[0]);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Error retrieving account information' });
-    }
-  }
-
-  async updateAccountInfo(req, res) {
-    const inputData = req.body;
-    const values = [
-      inputData.first_name,
-      inputData.last_name,
-      inputData.email,
-      inputData.phone,
-      inputData.userId,
-    ];
-    const query = 'UPDATE Users SET first_name = ?, last_name = ?, email = ?, phone = ? WHERE id = ?;';
-    try {
-      await this.executeQuery(query, values);
-      res.status(200).json({ message: 'Account information updated successfully' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Error updating account information' });
-    }
-  }
-
-  async executeQuery(query, params) {
-    return new Promise((resolve, reject) => {
-      db_conn.query(query, params, (error, results) => {
-        if (error) reject(error);
-        else resolve(results);
-      });
+const queryAsync = (sql, values) => {
+  return new Promise((resolve, reject) => {
+    db_conn.query(sql, values, (error, results) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(results);
+      }
     });
-  }
-}
+  });
+};
 
-module.exports = new accountController();
+const AccountController = {
+  getAccountInfo: async (userId) => {
+    try {
+      const query = 'SELECT first_name, last_name, email, phone FROM Users WHERE id = ?;';
+      const result = await queryAsync(query, [userId]);
+
+      if (result.length === 0) {
+        throw new Error('User not found');
+      }
+
+      return result[0];
+    } catch (error) {
+      console.error(error);
+      throw new Error('Account Details Retrieval Error');
+    }
+  },
+
+  updateAccountInfo: async (inputData) => {
+    try {
+      const values = [
+        inputData.first_name,
+        inputData.last_name,
+        inputData.email,
+        inputData.phone,
+        inputData.userId,
+      ];
+
+      const query = 'UPDATE Users SET first_name = ?, last_name = ?, email = ?, phone = ? WHERE id = ?;';
+      await db_conn.query(query, values);
+
+      return { message: 'Account information updated successfully' };
+    } catch (error) {
+      console.error('Error updating account information:', error);
+      throw new Error('Error updating account information');
+    }
+  },
+};
+
+module.exports = AccountController;
--- routes/accountSettings.js
+++ routes/accountSettings.js
@@ -1,39 +1,27 @@
 const express = require('express');
-const db_conn = require('../db_connection');
+const AccountController = require('../controllers/accountController');
 const router = express.Router();
 
-router.post('/get-account-info', (req, res) => {
-    const userId = req.body.userId;
-    const query = `SELECT first_name, last_name, email, phone from Users WHERE id = ?;`
-    db_conn.query(query, [userId], (error, result) => {
-        if(error){
-            console.error(error)
-            return res.status(500).json({ message: 'Error retrieving account information' });
-        }
-        res.status(200).json(result[0]);
-    });
-})
 
-router.post('/update-account-info', (req, res) => {
+router.post('/get-account-info', async (req, res) => {
+  try {
+    const userId = req.body.userId;
+    const accountInfo = await AccountController.getAccountInfo(userId);
+
+    res.status(200).json(accountInfo);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.post('/update-account-info', async (req, res) => {
+  try {
     const inputData = req.body;
-    const values = [
-        inputData.first_name,
-        inputData.last_name,
-        inputData.email,
-        inputData.phone,
-        inputData.userId,
-    ]
-    const query = 'UPDATE Users SET first_name = ?, last_name = ?, email = ?, phone = ? WHERE id = ?;'
-    db_conn.query(query, values, (error, result) => {
-        if(error){
-            console.error(error)
-            return res.status(500).json({ message: 'Error updating account information' });
-        }
-        res.status(200).json({ message: 'Account information updated successfully' });
-    });
-})
+    const result = await AccountController.updateAccountInfo(inputData);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 module.exports = router;
-
-
-
