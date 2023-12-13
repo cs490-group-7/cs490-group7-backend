cce0bb2664b456567c093a0941f2fa723d7fe83f
Sim10XXX <deox.master@gmail.com>
2023-12-13 14:33:24 -0500

    Add CoachRemoval table

--- sql/setupDB.sql
+++ sql/setupDB.sql
@@ -425,3 +425,16 @@ CREATE TABLE DailySurvey (
     PRIMARY KEY (user_id, date),
     FOREIGN KEY (user_id) REFERENCES Users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE CoachRemoval (
+    removal_id INT NOT NULL,
+    user_id INT NOT NULL,
+    coach_id INT NOT NULL,
+    reason TEXT,
+
+    last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (removal_id),
+    FOREIGN KEY (user_id) REFERENCES Users (id) ON DELETE CASCADE,
+    FOREIGN KEY (coach_id) REFERENCES Users (id) ON DELETE CASCADE
+);
