eb3b9e824156d5f0e4b796dd6e33522ddb2134e9
Sim10XXX <deox.master@gmail.com>
2023-12-13 14:33:56 -0500

    Remove duplicate route

--- routes/coachRoutes.js
+++ routes/coachRoutes.js
@@ -44,19 +44,6 @@ router.post('/client-progress', (req, res) => {
   });
 });
 
-router.post('/get-current-clients', (req, res) => {
-  const coachId = req.body.userId;
-  const query = `select coach_id, client_id, first_name, last_name from Coach_Request
-      inner join Users on Coach_Request.client_id = Users.id
-      where Coach_Request.coach_id = ? AND Coach_Request.accepted = TRUE;`
-  db_conn.query(query, [coachId], (error, result) => {
-      if(error){
-          console.error(error)
-          return res.status(500).json({ message: 'Error retrieving current clients' });
-      }
-      res.status(200).json(result);
-  });
-})
 
 // Route for coach lookup, fetching only approved coaches
 router.post('/coach-lookup', (req, res) => {
